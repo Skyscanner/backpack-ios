f21ef68d604f6b9d0ab6fd9163df2bd714605d18
danielnagy <daniel.nagy@skyscanner.net>
2018-02-27 16:47:14 +0000

    [No Jira] iOS color fix

--- gulpfile.js
+++ gulpfile.js
@@ -85,12 +85,14 @@ const generatePrefixedConst = ({ type, name, value }) => {
 const parseTokens = tokensData => {
   const colors = _.chain(tokensData.properties)
     .filter(({ type }) => type === 'color')
-    .map(({ value, name, ...rest }) => ({
-      value: parseColor(value),
-      name: name
-      .replace("color", "")
-      .toLowerCase(), ...rest
-    }))
+    .map(({ value, name, ...rest }) => {
+      const newName = name.replace('color', '');
+      return {
+        value: parseColor(value),
+        name: newName[0].toLowerCase() + newName.slice(1),
+        ...rest,
+      };
+    })
     .value();
 
   const emphazisedWeight = convertFontWeight(
