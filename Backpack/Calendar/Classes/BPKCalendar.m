61458c3e3a9b14f1f13c9bb40412d5ef63eaa2f2
Szabados Áron <aron.szabados@skyscanner.net>
2018-11-15 17:59:31 +0100

    changed appearance

--- Backpack/Calendar/Classes/BPKCalendar.m
+++ Backpack/Calendar/Classes/BPKCalendar.m
@@ -1,7 +1,11 @@
 #import "BPKCalendar.h"
+#import "BPKCalendarHeaderView.h"
+
+#import <Backpack/Color.h>
+#import <Backpack/Font.h>
 #import <FSCalendar/FSCalendar.h>
 
-@interface BPKCalendar () <FSCalendarDelegate, FSCalendarDataSource>
+@interface BPKCalendar () <FSCalendarDelegate, FSCalendarDelegateAppearance, FSCalendarDataSource>
 
 @property (nonatomic) FSCalendar *calendarView;
 
@@ -35,8 +39,26 @@
     self.translatesAutoresizingMaskIntoConstraints = NO;
     self.calendarView = [[FSCalendar alloc] initWithFrame:CGRectZero];
     self.calendarView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.calendarView.scrollDirection = FSCalendarScrollDirectionVertical;
+    self.calendarView.scrollEnabled = YES;
+    self.calendarView.pagingEnabled = NO;
+    self.calendarView.placeholderType = FSCalendarPlaceholderTypeNone;
     self.calendarView.delegate = self;
     self.calendarView.dataSource = self;
+    
+    NSDictionary<NSAttributedStringKey, id> *monthTextAttributes = [BPKFont attributesForFontStyle:BPKFontStyleTextLgEmphasized];
+    NSDictionary<NSAttributedStringKey, id> *weekdayTextAttributes = [BPKFont attributesForFontStyle:BPKFontStyleTextSm];
+    
+    FSCalendarAppearance *appearance = self.calendarView.appearance;
+    appearance.headerDateFormat = @"MMMM";
+    appearance.headerTitleFont = monthTextAttributes[NSFontAttributeName];
+    appearance.headerTitleColor = [BPKColor gray900];
+    appearance.weekdayFont = weekdayTextAttributes[NSFontAttributeName];
+    appearance.weekdayTextColor = weekdayTextAttributes[NSForegroundColorAttributeName];
+    appearance.todayColor = [BPKColor gray100];
+    appearance.titleTodayColor = [BPKColor gray900];
+    appearance.titleDefaultColor = [BPKColor gray900];
+    appearance.selectionColor = [BPKColor blue500];
 }
 
 - (void)addViews {
@@ -50,6 +72,32 @@
                                               [self.calendarView.leftAnchor constraintEqualToAnchor:self.leftAnchor],
                                               [self.calendarView.rightAnchor constraintEqualToAnchor:self.rightAnchor]
                                             ]];
+}
+
+#pragma mark - <FSCalendarDelegateAppearance>
+
+- (UIColor *)calendar:(FSCalendar *)calendar appearance:(FSCalendarAppearance *)appearance fillDefaultColorForDate:(NSDate *)date
+{
+    if([self isDateInToday:date]) {
+        return [UIColor clearColor];
+    }
+    return appearance.borderDefaultColor;
+}
+
+- (UIColor *)calendar:(FSCalendar *)calendar appearance:(FSCalendarAppearance *)appearance borderDefaultColorForDate:(NSDate *)date
+{
+    if([self isDateInToday:date]) {
+        return appearance.todayColor;
+    }
+    return appearance.borderDefaultColor;
+}
+
+#pragma mark - helpers
+
+- (BOOL)isDateInToday:(NSDate *)date
+{
+    NSCalendar *gregorian = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    return [gregorian isDateInToday:date];
 }
 
 @end
