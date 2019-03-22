753ab337d769c8264c3df8846dcd7cba64dc3b83
Hugo Tunius <h@tunius.se>
2019-03-22 16:12:21 +0000

    [BPK-2239] Add docs

--- Backpack/Theme/Classes/BPKThemeContainerController.h
+++ Backpack/Theme/Classes/BPKThemeContainerController.h
@@ -21,12 +21,38 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * A container view controller that manages the application of a theme for its hierarchy of
+ * views.
+ *
+ * The view controller hierarchy and thus view hierarchy contained in the controller is conditionally
+ * rendered inside a container. By leveraging the container relationship consumers can use
+ * `UIAppearance` to alter the look of all views rendered within the container.
+ */
 NS_SWIFT_NAME(ThemeContainerController) @interface BPKThemeContainerController : UIViewController
 
-- (instancetype)initWithThemeContainer:(UIView *)container rootViewController:(UIViewController *)rootViewController;
+/**
+ * Create an instance with a given container view and root view controller.
+ *
+ * @param container The container view to render the root view controller's view hierarchy inside.
+ * @param rootViewController The root view controller to be used as a child view controller.
+ * @return A configured instance than can further be contained or set to be the `rootViewController` of a `window`.
+ */
+- (instancetype)initWithThemeContainer:(UIView *)container rootViewController:(UIViewController *)rootViewController NS_DESIGNATED_INITIALIZER;
 
+/**
+ * Controls whether the theme defined in the container is currently active. The default value is `YES`.
+ * Changing the value of this property will immediately cause the view hierarchy to update to reflect the
+ * new state. As such it should only be called on the main thread.
+ */
 @property(nonatomic, assign, getter=isThemeActive) BOOL themeActive;
-@property(nonatomic,  strong) UIView *themeContainer;
+
+/**
+ * The theme container that is currently being used if the theme is active.
+ * Changing the value of this property will immediately cause the view hierarchy to update to reflect the
+ * new state. As such it should only be called on the main thread.
+ */
+@property(nonatomic, strong) UIView *themeContainer;
 @end
 
 NS_ASSUME_NONNULL_END
