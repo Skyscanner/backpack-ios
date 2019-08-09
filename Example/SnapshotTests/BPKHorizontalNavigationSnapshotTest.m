eaf061ddced819ad040bec9957a461735ad5bb98
George Gillams <george.gillams@skyscanner.net>
2019-08-09 13:07:50 +0100

    [BPK-2787] Add tests for extreme widths

--- Example/SnapshotTests/BPKHorizontalNavigationSnapshotTest.m
+++ Example/SnapshotTests/BPKHorizontalNavigationSnapshotTest.m
@@ -33,11 +33,14 @@ NS_ASSUME_NONNULL_BEGIN
     self.recordMode = NO;
 }
 
-- (UIView *)displayHorizontalNavigation:(BPKHorizontalNavigation *)horizontalNavigation {
+- (UIView *)displayHorizontalNavigation:(BPKHorizontalNavigation *)horizontalNavigation width:(CGFloat) width {
     UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
     view.backgroundColor = UIColor.whiteColor;
 
     CGSize fittingSize = [horizontalNavigation systemLayoutSizeFittingSize:CGSizeMake(1000, 1000)];
+    if(width > -1){
+        fittingSize.width = width;
+    }
     horizontalNavigation.frame = CGRectMake(0.0, 0.0, fittingSize.width, fittingSize.height);
 
     [view addSubview:horizontalNavigation];
@@ -56,7 +59,7 @@ NS_ASSUME_NONNULL_BEGIN
                                                                                             selected:0];
     horizontalNavigation.showsSelectedBar = YES;
 
-    UIView *view = [self displayHorizontalNavigation:horizontalNavigation];
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:-1.0];
     FBSnapshotVerifyView(view, nil);
 }
 
@@ -70,7 +73,7 @@ NS_ASSUME_NONNULL_BEGIN
                                                                                             selected:2];
     horizontalNavigation.showsSelectedBar = YES;
 
-    UIView *view = [self displayHorizontalNavigation:horizontalNavigation];
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:-1.0];
     FBSnapshotVerifyView(view, nil);
 }
 
@@ -84,7 +87,7 @@ NS_ASSUME_NONNULL_BEGIN
                                                                                             selected:0];
     horizontalNavigation.showsSelectedBar = NO;
 
-    UIView *view = [self displayHorizontalNavigation:horizontalNavigation];
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:-1.0];
     FBSnapshotVerifyView(view, nil);
 }
 
@@ -98,27 +101,55 @@ NS_ASSUME_NONNULL_BEGIN
                                                                                             selected:0];
     horizontalNavigation.showsSelectedBar = YES;
 
-    UIView *view = [self displayHorizontalNavigation:horizontalNavigation];
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:-1.0];
     FBSnapshotVerifyView(view, nil);
 }
 
 - (void)testWithTheming {
     NSArray<BPKHorizontalNavigationOption *> *options = @[
-        [[BPKHorizontalNavigationOption alloc] initWithName:@"Flights" iconName:BPKIconNameFlight tag:0],
-        [[BPKHorizontalNavigationOption alloc] initWithName:@"Hotels" iconName:BPKIconNameCars tag:1],
-        [[BPKHorizontalNavigationOption alloc] initWithName:@"Car hire" iconName:BPKIconNameHotels tag:2]
-    ];
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Flights" iconName:BPKIconNameFlight tag:0],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Hotels" iconName:BPKIconNameCars tag:1],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Car hire" iconName:BPKIconNameHotels tag:2]
+                                                          ];
     BPKHorizontalNavigation *horizontalNavigation = [[BPKHorizontalNavigation alloc] initWithOptions:options
                                                                                             selected:0];
     horizontalNavigation.showsSelectedBar = YES;
-
+    
     horizontalNavigation.selectedColor = UIColor.orangeColor;
     horizontalNavigation.fontMapping = [[BPKFontMapping alloc] initWithFamily:@"SnellRoundhand"
                                                               regularFontFace:@"SnellRoundhand"
                                                              semiboldFontFace:@"SnellRoundhand-Bold"
                                                                 heavyFontFace:@"SnellRoundhand-Black"];
+    
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:-1.0];
+    FBSnapshotVerifyView(view, nil);
+}
 
-    UIView *view = [self displayHorizontalNavigation:horizontalNavigation];
+- (void)testWide {
+    NSArray<BPKHorizontalNavigationOption *> *options = @[
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Flights" iconName:BPKIconNameFlight tag:0],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Hotels" iconName:BPKIconNameCars tag:1],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Car hire" iconName:BPKIconNameHotels tag:2]
+                                                          ];
+    BPKHorizontalNavigation *horizontalNavigation = [[BPKHorizontalNavigation alloc] initWithOptions:options
+                                                                                            selected:0];
+    horizontalNavigation.showsSelectedBar = YES;
+    
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:550];
+    FBSnapshotVerifyView(view, nil);
+}
+
+- (void)testNarrow {
+    NSArray<BPKHorizontalNavigationOption *> *options = @[
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Flights" iconName:BPKIconNameFlight tag:0],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Hotels" iconName:BPKIconNameCars tag:1],
+                                                          [[BPKHorizontalNavigationOption alloc] initWithName:@"Car hire" iconName:BPKIconNameHotels tag:2]
+                                                          ];
+    BPKHorizontalNavigation *horizontalNavigation = [[BPKHorizontalNavigation alloc] initWithOptions:options
+                                                                                            selected:0];
+    horizontalNavigation.showsSelectedBar = YES;
+    
+    UIView *view = [self displayHorizontalNavigation:horizontalNavigation width:200];
     FBSnapshotVerifyView(view, nil);
 }
 
