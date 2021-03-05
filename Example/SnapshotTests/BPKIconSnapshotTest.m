3867815ea96861b6637268af6f779b0f21dd6fee
Hugo Tunius <h@tunius.se>
2021-03-05 14:34:35 +0000

    [BPK-4425] Add snapshot tests for all icons

--- Example/SnapshotTests/BPKIconSnapshotTest.m
+++ Example/SnapshotTests/BPKIconSnapshotTest.m
@@ -18,7 +18,15 @@
 
 #import <Backpack/Color.h>
 #import <Backpack/Icon.h>
+#import <Backpack/Spacing.h>
 #import <FBSnapshotTestCase/FBSnapshotTestCase.h>
+
+
+typedef UIImageView* (^BPKIconMakeSmallIcon)(BPKSmallIconName);
+typedef UIImageView* (^BPKIconMakeLargeIcon)(BPKLargeIconName);
+typedef UIImageView* (^BPKIconMakeXlIcon)(BPKXlIconName);
+typedef UIImageView* (^BPKIconGenericMakeIcon)(id);
+
 
 @interface BPKIconSnapshotTest : FBSnapshotTestCase
 
@@ -46,6 +54,98 @@
     view.image = icon;
 
     FBSnapshotVerifyView(view, nil)
+}
+
+- (void)testAllSmallIcons {
+    NSArray<BPKSmallIconName> *allSmallIcons = [[self class] allSmallIconNames];
+
+    UIView *view = [[self class] generateSmallIconGridWithIcons:allSmallIcons makeIconView:^UIImageView *(BPKSmallIconName name) {
+        UIImage *icon = [BPKIcon smallTemplateIconNamed:name];
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, icon.size.width, icon.size.height)];
+        imageView.image = icon;
+
+        return imageView;
+    }];
+
+    FBSnapshotVerifyView(view, nil);
+}
+
+- (void)testAllLargeIcons {
+    NSArray<BPKSmallIconName> *allLargeIcons = [[self class] allSmallIconNames];
+
+    UIView *view = [[self class] generateLargeIconGridWithIcons:allLargeIcons makeIconView:^UIImageView *(BPKLargeIconName name) {
+        UIImage *icon = [BPKIcon largeTemplateIconNamed:name];
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, icon.size.width, icon.size.height)];
+        imageView.image = icon;
+
+        return imageView;
+    }];
+
+    FBSnapshotVerifyView(view, nil);
+}
+
++ (NSArray<BPKSmallIconName> *)allSmallIconNames {
+    NSArray *allIcons = [BPKIcon.iconMapping allKeys];
+    NSMutableArray *result = [NSMutableArray new];
+
+    for (NSString *icon in allIcons) {
+        if ([icon hasSuffix:@"-sm"]) {
+            BPKSmallIconName name = [icon substringToIndex:icon.length - 3];
+            [result addObject:name];
+        }
+    }
+
+    return [result copy];
+}
+
++ (NSArray<BPKLargeIconName> *)allLargeIconNames {
+    NSArray *allIcons = [BPKIcon.iconMapping allKeys];
+    NSMutableArray *result = [NSMutableArray new];
+
+    for (NSString *icon in allIcons) {
+        if (![icon hasSuffix:@"-sm"]) {
+            [result addObject:(BPKLargeIconName) icon];
+        }
+    }
+
+    return [result copy];
+}
+
++ (UIView *)generateSmallIconGridWithIcons:(NSArray<BPKSmallIconName> *)icons makeIconView:(BPKIconMakeSmallIcon)makeSmallIcon {
+    return [self generateIconGridWithIcons:icons
+                              makeIconView:makeSmallIcon
+                                  iconSize:BPKIcon.concreteSizeForSmallIcon];
+}
+
++ (UIView *)generateLargeIconGridWithIcons:(NSArray<BPKLargeIconName> *)icons makeIconView:(BPKIconMakeLargeIcon)makeLargeIcon {
+    return [self generateIconGridWithIcons:icons
+                              makeIconView:makeLargeIcon
+                                  iconSize:BPKIcon.concreteSizeForLargeIcon];
+}
+
++ (UIView *)generateIconGridWithIcons:(NSArray<id> *)icons makeIconView:(BPKIconGenericMakeIcon)makeIcon iconSize:(CGSize)iconSize {
+    NSUInteger iconsPerRow = 10;
+    CGFloat numberOfRows = ceil((CGFloat)icons.count / (CGFloat)iconsPerRow);
+    CGFloat width = iconsPerRow * iconSize.width + (iconsPerRow - 1) * BPKSpacingMd + BPKSpacingMd * 2;
+    CGFloat height = numberOfRows * iconSize.height + (numberOfRows - 1) * BPKSpacingMd + BPKSpacingMd * 2;
+
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, width, height)];
+
+    for (NSUInteger i = 0; i < icons.count; i++) {
+        id icon = icons[i];
+        NSUInteger row = i / iconsPerRow;
+        NSUInteger col = i % iconsPerRow;
+
+        UIImageView *imageView = makeIcon(icon);
+        imageView.tintColor = BPKColor.black;
+
+        CGFloat x = BPKSpacingMd + col * (iconSize.width + BPKSpacingMd);
+        CGFloat y = BPKSpacingMd + row * (iconSize.height + BPKSpacingMd);
+        imageView.frame = CGRectMake(x, y, iconSize.width, iconSize.height);
+        [view addSubview:imageView];
+    }
+
+    return view;
 }
 
 @end
