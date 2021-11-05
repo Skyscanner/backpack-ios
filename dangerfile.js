21e36223bffae68f293347714d93dc771d1a0b2a
Zhivko Georgiev <zhivko.georgiev@skyscanner.net>
2021-11-05 16:57:51 +0000

    Remove first time contributor greeting. This is due to GITHUB_TOKEN not having read:org permissions

--- dangerfile.js
+++ dangerfile.js
@@ -25,65 +25,15 @@ import fs from 'fs';
 
 import { danger, fail, warn, markdown, schedule } from 'danger';
 
-const getRandomFromArray = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
 const currentYear = new Date().getFullYear();
 // Applies to js, css, scss and sh files that are not located in dist or flow-typed folders.
 const shouldContainLicensingInformation = (filePath) =>
   filePath.match(/\.(js|sh|swift|h|m|njk)$/);
 
-const author = danger.github.pr.user.login;
-const isPrExternal = async () => {
-  try {
-    await danger.github.api.orgs.checkMembership({
-      org: 'backpack',
-      username: author,
-    });
-    return false;
-  } catch (err) {
-    return true;
-  }
-};
-
 const createdFiles = danger.git.created_files;
 const modifiedFiles = danger.git.modified_files;
 const fileChanges = [...modifiedFiles, ...createdFiles];
-
-const thanksGifs = [
-  'https://media.giphy.com/media/KJ1f5iTl4Oo7u/giphy.gif', // T.Hanks
-  'https://media.giphy.com/media/6tHy8UAbv3zgs/giphy.gif', // Spongebob
-  'https://media.giphy.com/media/xULW8v7LtZrgcaGvC0/giphy.gif', // Dog
-  'https://media.giphy.com/media/GghJ32T5oPR8Q/giphy.gif', // Leslie Knope
-  'https://media.giphy.com/media/26AHAw0aMmWwRI4Hm/giphy.gif', // David Mitchell
-  'https://media.giphy.com/media/mbhseRYedlG5W/giphy.gif', // That guy from Who's Line Is It Anyway who looks like Bill Murray
-  'https://media.giphy.com/media/3o6ZsXRBB9E67nUjL2/giphy.gif', // We love you
-  'https://media.giphy.com/media/l3V0sNZ0NGomeurCM/giphy.gif', // Bowie
-  'https://media.giphy.com/media/3rgXBvoeXt3MXlqhO0/giphy.gif', // Amazement
-  'https://media.giphy.com/media/1OnDp7RwgphjG/giphy.gif', // Kumamon
-  'https://media.giphy.com/media/1lk1IcVgqPLkA/giphy.gif', // Cap salute
-  'https://media.giphy.com/media/l3V0lN5bUX7AKtdW8/giphy.gif', // Moira Schitt's Creek
-  'https://media.giphy.com/media/3osxYdXvsGw6wT5lIY/giphy.gif', // OMG thanks
-  'https://media.giphy.com/media/nEMmyUp4hl1QOzovKh/giphy.gif', // thanks dude
-  'https://media.giphy.com/media/l0G192kSMLwTpO1CE/giphy.gif', // moe simpsons
-];
-
-// Be nice to our neighbours.
-schedule(async () => {
-  if (await isPrExternal()) {
-    markdown(`
-# Hi ${author}!
-
-Thanks for the PR 🎉! Contributions like yours help to improve the design system
-for everybody and we appreciate you taking the effort to create this PR.
-
-![Thanks](${getRandomFromArray(thanksGifs)})
-
-- [ ] Check this if you have read and followed the [contributing guidelines](https://github.com/Skyscanner/backpack-ios/blob/main/CONTRIBUTING.md)
-
-If you're curious about how we review, please read through the
-[code review guidelines](https://github.com/Skyscanner/backpack/blob/main/CODE_REVIEW_GUIDELINES.md).
-`);
-  }
-});
 
 const pbxprojFilePath = 'Example/Backpack.xcodeproj/project.pbxproj';
 
