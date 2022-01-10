ad9cd6723732be5424c3e0d498868570abcb4f36
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2022-01-10 15:01:17 +0800

    chore: address PR comments

--- Example/Backpack/AppDelegate.swift
+++ Example/Backpack/AppDelegate.swift
@@ -30,8 +30,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var isUITestingEnabled = false
     
-    // swiftlint:disable line_length
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // swiftlint:disable indentation_width
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.isUITestingEnabled = ProcessInfo.processInfo.arguments.contains("UITests")
         
         let relativeFontDefinition = BPKRelativeFontDefinition()
@@ -61,25 +62,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         BPKAppearance.apply()
         
-        // swiftlint:disable:next line_length
-        AppCenter.start(withAppSecret: "$(APP_CENTER_SECRET)", services: [Analytics.self, Crashes.self, Distribute.self])
-
+        AppCenter.start(withAppSecret: "$(APP_CENTER_SECRET)",
+                        services: [Analytics.self, Crashes.self, Distribute.self])
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
-    // swiftlint:disable:next line_length
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running,
-        // this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
