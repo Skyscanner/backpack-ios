a0bf24c94f21d91754588b07493eca3751e3f996
Nicolas Frugoni <nfrugoni19@gmail.com>
2022-02-23 12:26:02 +0000

    leaving only copyright begining year (#1194)

--- Example/Backpack/ViewControllers/StarRatingsViewController.swift
+++ Example/Backpack/ViewControllers/StarRatingsViewController.swift
@@ -2,7 +2,7 @@
 /*
  * Backpack - Skyscanner's Design System
  *
- * Copyright © 2019. Skyscanner Ltd. All rights reserved.
+ * Copyright 2018 Skyscanner Ltd. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
