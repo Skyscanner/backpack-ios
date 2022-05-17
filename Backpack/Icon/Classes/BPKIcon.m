e54132dd9c3e39851fbd8b4ba3c731beee8be963
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2022-05-17 10:33:45 +0800

    KOA-3425 move automirror code to top function

--- Backpack/Icon/Classes/BPKIcon.m
+++ Backpack/Icon/Classes/BPKIcon.m
@@ -33,11 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (UIImage *)templateIconNamed:(NSString *)name size:(BPKIconSize)size {
     UIImage *image = [self iconNamed:name color:UIColor.blackColor size:size];
-
-    if ([BPKAutoMirrorIconNames.items containsObject:name]) {
-        image = [image imageFlippedForRightToLeftLayoutDirection];
-    }
-
+    
     return [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
 }
 
@@ -73,6 +69,10 @@ NS_ASSUME_NONNULL_BEGIN
     [icon drawInRect:iconRect];
     icon = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
+    
+    if ([BPKAutoMirrorIconNames.items containsObject:name]) {
+        icon = [icon imageFlippedForRightToLeftLayoutDirection];
+    }
 
     [self.imageCache setObject:icon forKey:cacheKey];
 
