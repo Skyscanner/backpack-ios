714d34aac326e37381fdb503c3100dca01440aa6
Gert-Jan Vercauteren <gert-jan.vercauteren@skyscanner.net>
2022-08-25 10:39:35 +0800

    KOA-5485 update calendar colours

--- Backpack/Calendar/Classes/BPKCalendar.m
+++ Backpack/Calendar/Classes/BPKCalendar.m
@@ -871,9 +871,7 @@ CGFloat const BPKCalendarDefaultCellHeight = 44;
 }
 
 - (UIColor *)currentDateSelectedContentColor {
-    return self.dateSelectedContentColor != nil
-               ? self.dateSelectedContentColor
-               : [BPKColor dynamicColorWithLightVariant:BPKColor.textPrimaryDarkColor darkVariant:BPKColor.textPrimaryLightColor];
+    return self.dateSelectedContentColor != nil ? self.dateSelectedContentColor: BPKColor.textPrimaryInverseColor;
 }
 
 @end
