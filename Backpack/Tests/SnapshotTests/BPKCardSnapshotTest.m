b4e0a30069073d28fd4846b0fe76f2aaa619c533
Jeff Lau <115712286+JLau-Sky@users.noreply.github.com>
2022-10-19 10:52:59 +0100

    Turned off record mode

--- Backpack/Tests/SnapshotTests/BPKCardSnapshotTest.m
+++ Backpack/Tests/SnapshotTests/BPKCardSnapshotTest.m
@@ -34,7 +34,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)setUp {
     [super setUp];
-    self.recordMode = YES;
+    self.recordMode = NO;
 }
 
 - (void)configureParentView:(UIView *)parentView forCard:(BPKCard *)card {
