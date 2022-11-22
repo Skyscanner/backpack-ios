07c5408731c71901377179e08c35406f0b58ea03
Nicolas Frugoni <nfrugoni19@gmail.com>
2022-11-22 18:14:21 +0000

    spinner

---
+++ BPKSpinnerSnapshotTest.swift
@@ -0,0 +1,63 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import XCTest
+import Backpack
+import SnapshotTesting
+
+class BPKSpinnerSnapshotTest: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        isRecording = true
+    }
+    
+    private var allStyles: [BPKSpinnerStyle] = [
+        .textPrimary,
+        .disabled,
+        .onDarkSurface
+    ]
+    
+    private var allSizes: [BPKSpinnerSize] = [
+        .sm,
+        .lg,
+        .xl
+    ]
+    
+    private func createView(themed: Bool = false) -> UIView {
+        let view = viewsInStack(withStyles: allStyles) { style in
+            viewsInStack(withStyles: allSizes) { size in
+                let spinner = BPKSpinner(style: style, size: size)
+                if themed {
+                    spinner.primaryColor = BPKColor.coreEcoColor
+                }
+                spinner.hidesWhenStopped = false
+                return spinner
+            }
+        }
+        
+        return view
+    }
+    
+    func testSpinnerSizesNotThemed() {
+        assertSnapshot(createView())
+    }
+    
+    func testSpinnerSizesThemed() {
+        assertSnapshot(createView(themed: true))
+    }
+}
