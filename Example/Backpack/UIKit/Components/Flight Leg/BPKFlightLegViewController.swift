7f0410f213e5e23804b760445cc5527c06e953a8
Jeff Lau <115712286+JLau-Sky@users.noreply.github.com>
2023-04-06 15:16:49 +0100

    Using UIImageView instead of BPKIconView for airline logo (#1628)
    * Using UIImageView instead of BPKIconView for airline logo
    
    * Updated with PR comments
    
    * Fixed snapshot colours
    
    * Used correct color value
    
    * Updated snapshot after color change

--- Example/Backpack/UIKit/Components/Flight Leg/BPKFlightLegViewController.swift
+++ Example/Backpack/UIKit/Components/Flight Leg/BPKFlightLegViewController.swift
@@ -81,7 +81,7 @@ class BPKFlightLegViewController: UIViewController {
         duration: String,
         operatedBy: String? = nil,
         warning: String? = nil,
-        carrierLogo: BPKIconName
+        carrierLogo: UIImage? = nil
     ) -> BPKFlightLeg {
         
         return BPKFlightLeg(
@@ -103,7 +103,7 @@ class BPKFlightLegViewController: UIViewController {
             flightDescription: defaultDescription,
             stopsInfo: "Direct",
             duration: "7h 55m",
-            carrierLogo: .airline
+            carrierLogo: BPKIcon.makeLargeIcon(name: .airline, color: BPKColor.textOnLightColor)
         )
     }
     
@@ -117,7 +117,7 @@ class BPKFlightLegViewController: UIViewController {
             duration: "7h 55m",
             operatedBy: "Operated by WestJet",
             warning: "Change airports in London",
-            carrierLogo: .airlineMultiple
+            carrierLogo: BPKIcon.makeLargeIcon(name: .airlineMultiple, color: BPKColor.textOnLightColor)
         )
     }
     
@@ -132,7 +132,7 @@ class BPKFlightLegViewController: UIViewController {
             operatedBy: "Operated by WestJet Operated by WestJet Operated" +
             "by WestJet Operated by WestJet Operated by WestJet",
             warning: "Change airports in London",
-            carrierLogo: .airlineMultiple
+            carrierLogo: BPKIcon.makeLargeIcon(name: .airlineMultiple, color: BPKColor.textOnLightColor)
         )
     }
     
@@ -146,7 +146,7 @@ class BPKFlightLegViewController: UIViewController {
             duration: "7h 55m",
             operatedBy: "Operated by WestJet",
             warning: "Change airports in London",
-            carrierLogo: .airlineMultiple
+            carrierLogo: BPKIcon.makeLargeIcon(name: .airlineMultiple, color: BPKColor.textOnLightColor)
         )
     }
     
