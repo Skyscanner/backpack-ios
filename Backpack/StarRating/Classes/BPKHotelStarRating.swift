ab4856ccca124d892b75db325b471cecb031ac74
Jiahao Deng <mail@ihandle.top>
2023-06-13 17:00:00 +0800

    add missing parentheses

--- Backpack/StarRating/Classes/BPKHotelStarRating.swift
+++ Backpack/StarRating/Classes/BPKHotelStarRating.swift
@@ -9,6 +9,7 @@
  * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -78,7 +79,7 @@ public class BPKHotelStarRating: UIView {
     }
     
     private func updateLookAndFeel() {
-        stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        stackView.arrangedSubviews.forEach({ $0.removeFromSuperview() })
         
         guard rating > 0 else { return }
         
