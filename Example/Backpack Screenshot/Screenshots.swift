5eca0323a0823f6abfe65d4cea557ac29efe78b6
Nicolas Frugoni <nfrugoni19@gmail.com>
2023-07-28 13:06:56 +0100

    duplicated screenshots

--- Example/Backpack Screenshot/Screenshots.swift
+++ Example/Backpack Screenshot/Screenshots.swift
@@ -44,7 +44,7 @@ class Screenshots: BackpackSnapshotTestCase {
        app = createApp()
        setupSnapshot(app)
        app.launch()
-//       captureAllScreenshots()
+      captureAllScreenshots()
     }
 
     func testDarkModeScreenshots() {
@@ -52,7 +52,7 @@ class Screenshots: BackpackSnapshotTestCase {
        app.launchArguments.append("FORCE_DARK_MODE")
        setupSnapshot(app)
        app.launch()
-//       captureAllScreenshots(userInterfaceStyle: .dark)
+      captureAllScreenshots(userInterfaceStyle: .dark)
     }
 
     func captureAllScreenshots(userInterfaceStyle: UIUserInterfaceStyle = .light) {
