37097a40026044ff0482031f0d72355d5b4137ba
Nicolas Frugoni <nfrugoni19@gmail.com>
2023-08-04 12:35:21 +0100

    poi

--- Backpack-SwiftUI/MapMarker/Classes/BPKPriceMapMarker.swift
+++ Backpack-SwiftUI/MapMarker/Classes/BPKPriceMapMarker.swift
@@ -97,14 +97,12 @@ public struct BPKPriceMapMarker: View {
                     .frame(width: 16, height: 6)
             }
         } else {
-            VStack(spacing: BPKSpacing.none) {
             BPKText(price, style: state.fontStyle)
                 .foregroundColor(state.foregroundColor)
                 .padding(.bottom, 6)
                 .padding(.vertical, 4)
                 .padding(.horizontal, .md)
                 .background(state.backgroundColor)
-            }
         }
     }
 }
@@ -117,6 +115,96 @@ public struct BPKPointerMapMarker: View {
             .background(Circle().foregroundColor(.coreAccentColor))
             .frame(width: .base, height: .base)
             
+    }
+}
+
+public struct BPKPoiMapMarker: View {
+    enum State {
+        case `default`
+        case focused
+        case disabled
+        
+        var fontStyle: BPKFontStyle {
+            switch self {
+            case .default:
+                return .label2
+            case .focused:
+                return .label1
+            case .disabled:
+                return .label2
+            }
+        }
+        
+        var foregroundColor: BPKColor {
+            switch self {
+            case .default:
+                return .textPrimaryInverseColor
+            case .focused:
+                return .coreAccentColor
+            case .disabled:
+                return .textDisabledColor
+            }
+        }
+        
+        var backgroundColor: BPKColor {
+            switch self {
+            case .default:
+                return .coreAccentColor
+            case .focused:
+                return .surfaceDefaultColor
+            case .disabled:
+                return .surfaceDefaultColor
+            }
+        }
+    }
+    
+    let state: State
+    let icon: BPKIcon
+    
+    public var body: some View {
+        BPKIconView(icon)
+            .background(state.backgroundColor)
+            .clipShape(BPKPOIMapMarkerShape())
+    }
+}
+
+/// A view that clips its content to a shape with a flare at one end.
+struct BPKPOIMapMarkerShape: Shape {
+    /// Creates a path with a flare at one end.
+    func path(in rect: CGRect) -> Path {
+        let size = rect.size
+        return Path { path in
+            let contentRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            path.addRoundedRect(
+                in: contentRect,
+                cornerSize: CGSize(width: 5, height: 5))
+        }
+    }
+    
+    /// Appends a flare to the path.
+    /// - Parameters:
+    ///   - path: The path to append the flare to.
+    ///   - size: The size of the bounding rectangle of the path.
+    ///   - flareHeight: The height of the flare.
+    private func appendFlare(to path: inout Path, size: CGSize, flareHeight: CGFloat) {
+        
+        // The scale factor to adjust the size of the flare
+        
+        let startPointY = size.height - flareHeight
+        
+        let centerPoint = (size.width / 2)
+        path.move(to: CGPoint(x: centerPoint - 8, y: startPointY))
+        
+        let courvePoint: CGFloat = 2
+        path.addLine(to: CGPoint(x: centerPoint - courvePoint, y: size.height - courvePoint / 2))
+        
+        let center = CGPoint(x: centerPoint + courvePoint, y: size.height - courvePoint / 2)
+        let curveCenter1 = CGPoint(x: centerPoint - courvePoint / 2, y: startPointY + flareHeight)
+        let curveCenter2 = CGPoint(x: centerPoint + courvePoint / 2, y: startPointY + flareHeight)
+        path.addCurve(to: center, control1: curveCenter1, control2: curveCenter2)
+        
+        path.addLine(to: CGPoint(x: centerPoint + 8, y: startPointY))
+        
     }
 }
 
@@ -205,10 +293,15 @@ struct BPKFlareViewMap<Content: View>: View {
 
 struct BPKPriceMapMarker_Previews: PreviewProvider {
     struct Annotation: Identifiable {
-        let id: String
-        let price: String
+        // swiftlint:disable nesting
+        enum Marker {
+            case price(String, BPKPriceMapMarker.State)
+            case poi(BPKIcon, BPKPoiMapMarker.State)
+            case pointer
+        }
+        let id = UUID()
+        let marker: Marker
         let coordinate: CLLocationCoordinate2D
-        let state: BPKPriceMapMarker.State
     }
     
     static var previews: some View {
@@ -219,41 +312,46 @@ struct BPKPriceMapMarker_Previews: PreviewProvider {
             )),
             annotationItems: [
                 Annotation(
-                    id: "1",
-                    price: "£200",
-                    coordinate: .init(latitude: 51.55, longitude: -0.1),
-                    state: .default
+                    marker: .price("£200", .default),
+                    coordinate: .init(latitude: 51.55, longitude: -0.1)
                 ),
                 Annotation(
-                    id: "2",
-                    price: "£250",
-                    coordinate: .init(latitude: 51.53, longitude: -0.1),
-                    state: .focused
+                    marker: .price("£200", .focused),
+                    coordinate: .init(latitude: 51.53, longitude: -0.1)
                 ),
                 Annotation(
-                    id: "3",
-                    price: "£90",
-                    coordinate: .init(latitude: 51.51, longitude: -0.1),
-                    state: .viewed
+                    marker: .price("£200", .viewed),
+                    coordinate: .init(latitude: 51.51, longitude: -0.1)
                 ),
                 Annotation(
-                    id: "4",
-                    price: "Sold out",
-                    coordinate: .init(latitude: 51.49, longitude: -0.1),
-                    state: .disabled
+                    marker: .price("Sold out", .disabled),
+                    coordinate: .init(latitude: 51.49, longitude: -0.1)
                 ),
                 Annotation(
-                    id: "5",
-                    price: "",
-                    coordinate: .init(latitude: 51.47, longitude: -0.1),
-                    state: .default
+                    marker: .pointer,
+                    coordinate: .init(latitude: 51.47, longitude: -0.1)
+                ),
+                Annotation(
+                    marker: .poi(.landmark, .default),
+                    coordinate: .init(latitude: 51.55, longitude: -0.04)
+                ),
+                Annotation(
+                    marker: .poi(.landmark, .focused),
+                    coordinate: .init(latitude: 51.53, longitude: -0.04)
+                ),
+                Annotation(
+                    marker: .poi(.landmark, .disabled),
+                    coordinate: .init(latitude: 51.51, longitude: -0.04)
                 )
             ]) { item in
                 MapAnnotation(coordinate: item.coordinate) {
-                    if item.id == "5" {
+                    switch item.marker {
+                    case .price(let price, let state):
+                        BPKPriceMapMarker(state: state, price: price)
+                    case .poi(let icon, let state):
+                        BPKPoiMapMarker(state: state, icon: icon)
+                    case .pointer:
                         BPKPointerMapMarker()
-                    } else {
-                        BPKPriceMapMarker(state: item.state, price: item.price)
                     }
                 }
         }
