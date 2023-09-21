586c774541374d85a9269aa560267ee9b3157820
alvin-mok <alvin.mok@skyscanner.net>
2023-09-21 13:19:11 +0100

    clean up

--- Example/Backpack/AppDelegate.swift
+++ Example/Backpack/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private func setupAppearance() {
         UINavigationBar.appearance().tintColor = BPKColor.textPrimaryColor
         if #unavailable(iOS 16.0) {
-            // This line is added as a temp solution for BPKTextEditor to give the .surfaceDefaultColor for iOS 15 or below
+            // This line is added as a temp solution for BPKTextEditor to have .surfaceDefaultColor as background colour in dark mode for iOS 15 or below
             UITextView.appearance().backgroundColor = BPKColor.surfaceDefaultColor
         }
         BPKAppearance.apply()
