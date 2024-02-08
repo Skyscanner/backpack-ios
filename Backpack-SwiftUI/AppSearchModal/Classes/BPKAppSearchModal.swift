27639eb8a4bceb6bec55550171ed2197dab846d4
Michael Baldock <michael.baldock@skyscanner.net>
2024-02-08 13:55:44 +0000

    LMN-774 Exposed BPKAppSearchModal.TextFieldState, with options for default and 'clear' state with accessibility text

--- Backpack-SwiftUI/AppSearchModal/Classes/BPKAppSearchModal.swift
+++ Backpack-SwiftUI/AppSearchModal/Classes/BPKAppSearchModal.swift
@@ -25,7 +25,7 @@ public struct BPKAppSearchModal: View {
     let results: BPKAppSearchModalResults
     let closeAccessibilityLabel: String
     let onClose: () -> Void
-    private var textFieldState: BPKTextField.State = .default
+    private var textFieldState: TextFieldState = .default
     
     public init(
         title: String,
@@ -50,7 +50,7 @@ public struct BPKAppSearchModal: View {
             
             if results.showTextField {
                 BPKTextField(placeholder: inputHint, $inputText)
-                    .inputState(textFieldState)
+                    .inputState(textFieldState.inputState)
             }
         
             switch results {
@@ -92,7 +92,7 @@ public struct BPKAppSearchModal: View {
         .padding(.vertical, .md)
     }
     
-    public func inputState(_ state: BPKTextField.State) -> BPKAppSearchModal {
+    public func inputState(_ state: TextFieldState) -> BPKAppSearchModal {
         var result = self
         result.textFieldState = state
         return result
