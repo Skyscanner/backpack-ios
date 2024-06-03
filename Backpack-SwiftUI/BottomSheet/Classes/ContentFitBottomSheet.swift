80405bbef4a52402921136835f25af208338d0ae
Nicolas Frugoni <nfrugoni19@gmail.com>
2024-06-03 13:49:12 +0100

    Extracting ContentFitBottomSheet for reusability

---
+++ Backpack-SwiftUI/BottomSheet/Classes/ContentFitBottomSheet.swift
@@ -0,0 +1,69 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+
+@available(iOS 16.0, *)
+struct ContentFitBottomSheet<Content: View, Header: View>: View {
+    let header: () -> Header
+    let bottomSheetContent: () -> Content
+    
+    @State private var detentHeight: CGFloat = 0
+    
+    var body: some View {
+        VStack {
+            header()
+            bottomSheetContent()
+                .presentationDetents([.height(detentHeight)])
+        }
+        .presentationDragIndicator(.visible)
+        .readHeight()
+        .onPreferenceChange(HeightPreferenceKey.self) { height in
+            if let height {
+                self.detentHeight = height
+            }
+        }
+    }
+}
+
+private struct HeightPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat?
+    
+    static func reduce(value: inout CGFloat?, nextValue: () -> CGFloat?) {
+        guard let nextValue = nextValue() else { return }
+        value = nextValue
+    }
+}
+
+private struct ReadHeightModifier: ViewModifier {
+    private var sizeView: some View {
+        GeometryReader { geometry in
+            Color.clear.preference(key: HeightPreferenceKey.self, value: geometry.size.height)
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        content.background(sizeView)
+    }
+}
+
+private extension View {
+    func readHeight() -> some View {
+        self.modifier(ReadHeightModifier())
+    }
+}
