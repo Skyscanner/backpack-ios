0b66526bea551b9019cdd6a864b9b92eaea73c7b
Nicolas Frugoni <nfrugoni19@gmail.com>
2024-07-25 18:04:13 +0200

    chore: Update dependencies to latest versions

--- Backpack/Calendar/Classes/FSCalendar/FSCalendar.h
+++ Backpack/Calendar/Classes/FSCalendar/FSCalendar.h
@@ -1,19 +1,20 @@
-//
-//  FSCalendar.h
-//  FSCalendar
-//
-//  Created by Wenchao Ding on 29/1/15.
-//  Copyright © 2016 Wenchao Ding. All rights reserved.
-//
-//  https://github.com/WenchaoD
-//
-//  FSCalendar is a superior awesome calendar control with high performance, high customizablility and very simple usage.
-//
-//  @see FSCalendarDataSource
-//  @see FSCalendarDelegate
-//  @see FSCalendarDelegateAppearance
-//  @see FSCalendarAppearance
-//
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "FSCalendarAppearance.h"
 #import "FSCalendarCell.h"
--- Backpack/Calendar/Classes/FSCalendar/FSCalendarCollectionViewLayout.m
+++ Backpack/Calendar/Classes/FSCalendar/FSCalendarCollectionViewLayout.m
@@ -1,10 +1,20 @@
-//
-//  FSCalendarAnimationLayout.m
-//  FSCalendar
-//
-//  Created by dingwenchao on 1/3/16.
-//  Copyright © 2016 Wenchao Ding. All rights reserved.
-//
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2018 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "FSCalendarCollectionViewLayout.h"
 #import "FSCalendar.h"
