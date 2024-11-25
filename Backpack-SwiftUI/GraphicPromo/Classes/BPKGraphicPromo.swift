2687c6b8d018a78a4388b617f65ad0c8a55c60f4
Dario Roa <Dario.Roa@skyscanner.net>
2024-11-25 17:00:55 +0100

    [MUON-577] Tablet layout for BPKGraphicPromo

--- Backpack-SwiftUI/GraphicPromo/Classes/BPKGraphicPromo.swift
+++ Backpack-SwiftUI/GraphicPromo/Classes/BPKGraphicPromo.swift
@@ -20,11 +20,17 @@ import SwiftUI
 
 public struct BPKGraphicPromo: View {
     @Environment(\.sizeCategory) var sizeCategory
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     
     private struct Sponsor {
         let title: String
         let logo: Image?
         let accessibilityLabel: String
+    }
+
+    private struct Constants {
+        static let aspectRatioMobile: CGFloat = 3/4
+        static let aspectRatioTablet: CGFloat = 1.96
     }
     
     private let kicker: String?
@@ -33,6 +39,7 @@ public struct BPKGraphicPromo: View {
     private let image: Image
     
     private let type: `Type`
+    private let layoutType: LayoutType
     private let overlay: BPKOverlayType
     private let variant: Variant
     private let verticalAlignment: VerticalAlignment
@@ -44,9 +51,13 @@ public struct BPKGraphicPromo: View {
     
     // MARK: - Internal settings
     private let sponsorLogoHeight = 32.0
-    private let aspectRatio: CGFloat = 3/4
     private let padding = BPKSpacing.lg
     private let cornerRadius = BPKCornerRadius.md
+    
+    public enum LayoutType {
+        case mobile
+        case tablet
+    }
     
     public init(
         kicker: String? = nil,
@@ -54,6 +65,7 @@ public struct BPKGraphicPromo: View {
         subheadline: String? = nil,
         image: Image,
         type: `Type` = .button,
+        layoutType: LayoutType = .mobile,
         overlay: BPKOverlayType = .solid(.off),
         variant: Variant = .onDark,
         verticalAlignment: BPKGraphicPromo.VerticalAlignment = .top
@@ -64,6 +76,7 @@ public struct BPKGraphicPromo: View {
         self.image = image
             
         self.type = type
+        self.layoutType = layoutType
         self.overlay = overlay
         self.variant = variant
             
@@ -74,6 +87,7 @@ public struct BPKGraphicPromo: View {
         headline: String,
         image: Image,
         type: `Type` = .button,
+        layoutType: LayoutType = .mobile,
         overlay: BPKOverlayType = .linear(.high, .bottom),
         variant: Variant = .onDark,
         sponsorTitle: String,
@@ -86,6 +100,7 @@ public struct BPKGraphicPromo: View {
         self.subheadline = nil
         self.image = image
         self.type = type
+        self.layoutType = layoutType
         self.overlay = overlay
         self.variant = variant
         
@@ -94,6 +109,8 @@ public struct BPKGraphicPromo: View {
             logo: partnerLogo,
             accessibilityLabel: sponsoredAccessibilityLabel)
     }
+    
+    @State private var containerWidth: CGFloat = 0
     
     public var body: some View {
         Button(action: tapAction) {
@@ -108,7 +125,7 @@ public struct BPKGraphicPromo: View {
             cornerRadius: cornerRadius
         ))
         .if(sizeCategory < .accessibilityExtraLarge, transform: {
-            $0.aspectRatio(aspectRatio, contentMode: .fit)
+            $0.aspectRatio(aspectRatio(), contentMode: .fit)
         })
         .accessibilityElement(children: .ignore)
         .accessibilityLabel(contentAccessibilityLabel)
@@ -160,7 +177,7 @@ public struct BPKGraphicPromo: View {
     }
     
     private var headlineView: some View {
-        BPKText(headline, style: .heading2)
+        BPKText(headline, style: layoutType == .mobile ? .heading2 : .heading1)
             .foregroundColor(variant.foregroundColor)
             .lineLimit(nil)
     }
@@ -230,6 +247,15 @@ public struct BPKGraphicPromo: View {
             perform()
         }
         return result
+    }
+    
+    private func aspectRatio() -> CGFloat {
+        switch layoutType {
+        case .mobile:
+            return Constants.aspectRatioMobile
+        case .tablet:
+            return Constants.aspectRatioTablet
+        }
     }
 }
 
@@ -301,6 +327,7 @@ struct BPKGraphicPromo_Previews: PreviewProvider {
                 BPKGraphicPromo(
                     headline: "There's always more to explore in Britain",
                     image: Image(systemName: "heart"),
+                    layoutType: .tablet,
                     sponsorTitle: "In partnership with Skyland",
                     partnerLogo: Image(systemName: "heart.fill"),
                     sponsoredAccessibilityLabel: "Sponsored by: Skyland"
