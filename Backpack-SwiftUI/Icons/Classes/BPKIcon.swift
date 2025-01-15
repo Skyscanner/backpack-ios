ddefa96ccba1d5ffdb0f22594c097a7a89054086
pablo-rodriguez <pablo.rodriguez@skyscanner.net>
2025-01-15 11:20:36 +0100

    import foundation on BPKIcon

--- Backpack-SwiftUI/Icons/Classes/BPKIcon.swift
+++ Backpack-SwiftUI/Icons/Classes/BPKIcon.swift
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import Foundation
 import Backpack_Common
 
 public struct BPKIcon {
