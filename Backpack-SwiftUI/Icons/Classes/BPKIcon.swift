18d45325ad24f5e239642644d0f8dc21fbbde702
pablo-rodriguez <pablo.rodriguez@skyscanner.net>
2025-01-15 12:21:13 +0100

    Update BPKIcon.swift

--- Backpack-SwiftUI/Icons/Classes/BPKIcon.swift
+++ Backpack-SwiftUI/Icons/Classes/BPKIcon.swift
@@ -15,8 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Foundation
-import Backpack_Common
+//import Foundation
+//import Backpack_Common
 
 public struct BPKIcon {
     public let name: String
