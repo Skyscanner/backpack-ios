0a7f1d9413e5186a222b47c04701e3f898bafee2
Kynan Song <kynan.song@skyscanner.net>
2025-09-11 14:46:46 +0100

    Removing import.

--- Backpack-SwiftUI/Text/Classes/BPKText.swift
+++ Backpack-SwiftUI/Text/Classes/BPKText.swift
@@ -17,7 +17,6 @@
  */
 
 import SwiftUI
-import Backpack_Common
 
 /// A view that displays one or more lines of text
 /// By default the color of BPKText is set to `BPKColor.textPrimary` with a lineLimit of 1
