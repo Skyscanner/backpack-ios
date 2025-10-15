65fadcb9f2d9a918f5a0c5c0d9cb2e112065dcd0
Georgios Ntogramatzis <georgios.ntogramatzis@skyscanner.net>
2025-10-15 15:43:09 +0200

    first step

---
+++ Backpack-SwiftUI/SponsoredInsetBanner/Classes/BPKSponsoredInsetBanner.swift
@@ -0,0 +1,185 @@
+//
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright © 2023 Skyscanner Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+
+public struct BPKSponsoredInsetBanner<LogoContent: View>: View {
+    private let logo: LogoContent?
+    private let title: String?
+    private let subheadline: String?
+    private let callToAction: CallToAction?
+    private let variant: Variant
+    private let backgroundColor: Color
+    private let customAccessibilityLabel: String?
+    
+    public init(
+        logoContent: LogoContent? = nil,
+        title: String,
+        subheadline: String? = nil,
+        callToAction: CallToAction? = nil,
+        variant: Variant,
+        backgroundColor: Color,
+        customAccessibilityLabel: String? = nil
+    ) {
+        self.logo = logoContent
+        self.title = title
+        self.subheadline = subheadline
+        self.callToAction = callToAction
+        self.variant = variant
+        self.backgroundColor = backgroundColor
+        self.customAccessibilityLabel = customAccessibilityLabel
+    }
+    
+    public init(
+        logoContent: LogoContent? = nil,
+        subheadline: String,
+        callToAction: CallToAction? = nil,
+        variant: Variant,
+        backgroundColor: Color,
+        customAccessibilityLabel: String? = nil
+    ) {
+        self.logo = logoContent
+        self.title = nil
+        self.subheadline = subheadline
+        self.callToAction = callToAction
+        self.variant = variant
+        self.backgroundColor = backgroundColor
+        self.customAccessibilityLabel = customAccessibilityLabel
+    }
+    
+    public init(
+        logoContent: LogoContent,
+        callToAction: CallToAction? = nil,
+        variant: Variant,
+        backgroundColor: Color,
+        customAccessibilityLabel: String? = nil
+    ) {
+        self.logo = logoContent
+        self.title = nil
+        self.subheadline = nil
+        self.callToAction = callToAction
+        self.variant = variant
+        self.backgroundColor = backgroundColor
+        self.customAccessibilityLabel = customAccessibilityLabel
+    }
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            topView
+                .background(backgroundColor)
+                .zIndex(1)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: .sm, style: .continuous)
+                .foregroundColor(.sponsoredBannerBackgroundColor)
+        )
+        .clipShape(
+            RoundedRectangle(cornerRadius: .sm, style: .continuous)
+        )
+        .if(customAccessibilityLabel != nil) { view in
+            view
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel(customAccessibilityLabel ?? "")
+        }
+    }
+    
+    private var topView: some View {
+        HStack() {
+            buttonContent
+        }
+        .accessibilityElement(children: .combine)
+        .buttonStyle(SponsoredInsetBannerButtonStyle(
+            foregroundColor: (variant == .onDark) ? Color(BPKColor.textOnDarkColor) : Color(BPKColor.textOnLightColor),
+            backgroundColor: backgroundColor
+        ))
+    }
+    
+    private var buttonContent: some View {
+        HStack(spacing: .md) {
+            titlesView
+            Spacer()
+            if let callToAction = callToAction {
+                VStack {
+                    HStack {
+                        BPKText(callToAction.text, style: .caption)
+                            .foregroundColor(variant.color)
+                            .lineLimit(nil)
+                        if callToAction.showIcon {
+                            iconView
+                        }
+                    }
+                    Spacer()
+                }
+                
+            }
+            
+        }
+        .padding(.base)
+    }
+    
+    private var titlesView: some View {
+        VStack(alignment: .leading, spacing: .sm) {
+            if let logo = logo {
+                logo
+                    .frame(maxWidth: Constants.maxLogoWidth, maxHeight: Constants.maxLogoHeight)
+                    .fixedSize(horizontal: true, vertical: false)
+            }
+            if let title = title {
+                BPKText(title, style: .label2)
+                    .foregroundColor(variant.color)
+                    .lineLimit(nil)
+            }
+            if let subheadline = subheadline {
+                BPKText(subheadline, style: .caption)
+                    .foregroundColor(variant.color)
+                    .lineLimit(nil)
+            }
+        }
+    }
+    
+    private var iconView: some View {
+        BPKIconView(.informationCircle)
+            .foregroundColor(variant.color)
+            .if(callToAction?.accessibilityHint != nil) { icon in
+                icon
+                    .accessibilityElement()
+                    .accessibilityHint(callToAction?.accessibilityHint ?? "")
+            }
+    }
+}
+
+private enum Constants {
+    static let maxLogoWidth: CGFloat = 88.0
+    static let maxLogoHeight: CGFloat = 22.0
+}
+
+struct BPKSponsoredInsetBanner_Previews: PreviewProvider {
+    static var previews: some View {
+        BPKSponsoredInsetBanner<EmptyView>(
+            title: "Title",
+            subheadline: "Subheading",
+            callToAction: .init(
+                text: "Sponsored",
+                accessibilityHint: "Double tap for more information",
+                showIcon: true),
+            variant: .onDark,
+            backgroundColor: Color(red: 1.000, green: 0.400, blue: 0.004, opacity: 1.000))
+        .padding(.base)
+    }
+}
---
+++ Example/Backpack/SwiftUI/Components/SponsoredInsetBanner/SponsoredInsetBannerExampleView.swift
@@ -0,0 +1,92 @@
+//
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright © 2023 Skyscanner Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+import Backpack_SwiftUI
+
+struct SponsoredInsetBannerExampleView: View {
+    private let orangeColor = Color(red: 1.000, green: 0.400, blue: 0.004, opacity: 1.000)
+    private let yellowColor = Color(red: 1.000, green: 0.890, blue: 0.000, opacity: 1.000)
+    var body: some View {
+        ScrollView {
+            VStack(spacing: .base) {
+                titleOnlyView
+                allTitlesView
+                onLightView
+                sponsoredWithoutCTAOnLightView
+                imageAndCTAView
+            }
+            .padding(.base)
+        }
+    }
+    
+    private var titleOnlyView: some View {
+        BPKSponsoredInsetBanner<EmptyView>(
+            title: "Title",
+            variant: .onDark,
+            backgroundColor: orangeColor)
+    }
+    
+    private var allTitlesView: some View {
+        BPKSponsoredInsetBanner<EmptyView>(
+            title: "Title",
+            subheadline: "Subheadline",
+            variant: .onDark,
+            backgroundColor: orangeColor)
+    }
+    
+    private var imageAndCTAView: some View {
+        BPKSponsoredInsetBanner(
+            logoContent: Image("skyland").resizable().scaledToFit(),
+            title: "Title",
+            subheadline: "Subheading",
+            callToAction: .init(
+                text: "Sponsored",
+                accessibilityHint: "Double tap for more information"),
+            variant: .onDark,
+            backgroundColor: orangeColor,
+        )
+    }
+    
+    private var onLightView: some View {
+        BPKSponsoredInsetBanner<EmptyView>(
+            title: "On Light variant",
+            subheadline: "Subheadline",
+            variant: .onLight,
+            backgroundColor: yellowColor)
+    }
+    
+    private var sponsoredWithoutCTAOnLightView: some View {
+        BPKSponsoredInsetBanner<EmptyView>(
+            title: "On Light Variant",
+            subheadline: "Without info icon",
+            callToAction: .init(
+                text: "Sponsored",
+                accessibilityHint: "Double tap for more information",
+                showIcon: false),
+            variant: .onLight,
+            backgroundColor: yellowColor)
+    }
+}
+
+struct SponsoredInsetBannerExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SponsoredInsetBannerExampleView()
+    }
+}
