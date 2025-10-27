e7c50eb2af0bd6c27a7688bf84ce2a7c59e0b845
ilkay <ilkay.hamit@gmail.com>
2025-10-27 15:33:48 +0000

    DON-2259 bpkdivided card swiftui wrapper

---
+++ Backpack-SwiftUI/Card/Classes/UIView+SwiftUIDividedCard.swift
@@ -0,0 +1,157 @@
+/*
+ * Backpack - Skyscanner's Design System
+ *
+ * Copyright 2025 Skyscanner Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// swiftlint:disable indentation_width
+import SwiftUI
+
+
+public struct BPKDividedCardSwiftUIWrapper: View {
+    private let elevation: BPKCardElevation
+    private let primaryContent: UIView
+    private let secondaryContent: UIView
+    private var tapAction: () -> Void = {}
+    private let alignment: HorizontalAlignment
+
+    public init(elevation: BPKCardElevation = .default,
+                alignment: HorizontalAlignment = .leading,
+                primaryContent: UIView,
+                secondaryContent: UIView,
+                tapAction: @escaping (() -> Void)) {
+        self.elevation = elevation
+        self.alignment = alignment
+        self.primaryContent = primaryContent
+        self.secondaryContent = secondaryContent
+        self.tapAction = tapAction
+    }
+
+    public var body: some View {
+        BPKDividedCard(alignment: alignment, elevation: elevation) {
+            HostedView(view: primaryContent)
+        } secondaryContent: {
+            HostedView(view: secondaryContent)
+        }
+        .onTapGesture {
+            tapAction()
+        }
+
+    }
+}
+
+private struct HostedView: UIViewRepresentable {
+    let view: UIView
+
+    func makeUIView(context: Context) -> HostedUIViewContainer {
+        let container = HostedUIViewContainer()
+        container.host(view)
+        return container
+    }
+
+    func updateUIView(_ uiView: HostedUIViewContainer, context: Context) {
+        uiView.host(view)
+    }
+
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: HostedUIViewContainer, context: Context) -> CGSize? {
+        guard let width = proposal.width else {
+            return nil
+        }
+
+        let height = proposal.height ?? .infinity
+        let size = uiView.sizeThatFits(CGSize(width: width, height: height))
+        return size
+    }
+}
+
+private final class HostedUIViewContainer: UIView {
+    private weak var hostedView: UIView?
+    private var hostedConstraints: [NSLayoutConstraint] = []
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+
+    func host(_ view: UIView) {
+        guard hostedView !== view else {
+            invalidateIntrinsicContentSize()
+            return
+        }
+
+        hostedView = view
+        view.removeFromSuperview()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(view)
+
+        // Pin all edges to ensure the view fills this container
+        hostedConstraints = [
+            view.topAnchor.constraint(equalTo: topAnchor),
+            view.leadingAnchor.constraint(equalTo: leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ]
+
+        NSLayoutConstraint.activate(hostedConstraints)
+
+        invalidateIntrinsicContentSize()
+    }
+
+    override var bounds: CGRect {
+        didSet {
+            // Only trigger layout if bounds actually changed significantly
+            // This prevents infinite loops from temporary bounds changes in sizeThatFits
+            guard abs(oldValue.width - bounds.width) > 0.5 ||
+                  abs(oldValue.height - bounds.height) > 0.5 else {
+                return
+            }
+            hostedView?.setNeedsLayout()
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        hostedView?.layoutIfNeeded()
+    }
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let hostedView else {
+            return .zero
+        }
+
+        let width = size.width > 0 ? size.width : bounds.width
+
+        let fittingSize = hostedView.systemLayoutSizeFitting(
+            CGSize(width: width, height: UIView.layoutFittingCompressedSize.height),
+            withHorizontalFittingPriority: .required,
+            verticalFittingPriority: .fittingSizeLevel
+        )
+
+        return fittingSize
+    }
+
+    private func configureView() {
+        backgroundColor = .clear
+        clipsToBounds = true
+        setContentHuggingPriority(.required, for: .vertical)
+        setContentCompressionResistancePriority(.required, for: .vertical)
+    }
+}
