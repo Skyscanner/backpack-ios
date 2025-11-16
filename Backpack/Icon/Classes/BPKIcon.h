1c239a2e25b3e40bedc727c96836c6847cdaa6c1
Soheil Novinfard <soheil.novinfard@skyscanner.net>
2025-11-16 12:55:17 +0000

    Add UIKit import and update header search paths
    Imported UIKit in BPKIcon.h to ensure UIImage is available and removed the forward declaration of UIImage. Updated Package.swift to include Icon/Classes/Generated in the header search paths for proper header resolution.

--- Backpack/Icon/Classes/BPKIcon.h
+++ Backpack/Icon/Classes/BPKIcon.h
@@ -16,6 +16,7 @@
  * limitations under the License.
  */
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import "BPKIconNames.h"
 #import "BPKLargeIconNames.h"
@@ -41,8 +42,6 @@ typedef NS_ENUM(NSUInteger, BPKIconSize) {
      */
     BPKIconSizeLarge,
 };
-
-@class UIImage;
 
 /**
  * This class encapsulates all Backpack icons and the ability to render them as `UIImage`. It
