323102e57c328704faef2d43d6191b2e8e5a1027
Jasper Sion <jasper.sion@skyscanner.net>
2026-08-04 15:47:11 +0100

    [DON-3412] Improve BPKSectionHeader accessibility tests

--- Backpack-SwiftUI/Tests/SectionHeader/BPKSectionHeaderViewTests.swift
+++ Backpack-SwiftUI/Tests/SectionHeader/BPKSectionHeaderViewTests.swift
@@ -18,7 +18,7 @@
 
 import XCTest
 import SnapshotTesting
-import Backpack_SwiftUI
+@testable import Backpack_SwiftUI
 
 class BPKSectionHeaderViewTests: XCTestCase {
     func testSectionHeaderDefault() {
@@ -77,13 +77,34 @@ class BPKSectionHeaderViewTests: XCTestCase {
         )
     }
 
-    func testSectionHeaderWithoutAccessibilityHeaderTrait() {
-        assertSnapshot(
-            BPKSectionHeader(
-                title: "Section title",
-                accessibilityHeaderEnabled: false
-            )
-            .padding()
+    func testSectionHeaderAccessibilityHeaderEnabledByDefault() {
+        let sectionHeader = BPKSectionHeader(
+            title: "Section title"
         )
+
+        XCTAssertTrue(sectionHeader.accessibilityHeaderEnabled)
+    }
+
+    func testSectionHeaderAccessibilityHeaderCanBeDisabled() {
+        let sectionHeader = BPKSectionHeader(
+            title: "Section title",
+            accessibilityHeaderEnabled: false
+        )
+
+        XCTAssertFalse(sectionHeader.accessibilityHeaderEnabled)
+    }
+
+    func testSectionHeaderWithButtonAccessibilityHeaderCanBeDisabled() {
+        let sectionHeader = BPKSectionHeader(
+            title: "Section title",
+            accessibilityHeaderEnabled: false
+        ) {
+            BPKButton(
+                icon: .addCircle,
+                accessibilityLabel: "Add item"
+            ) { }
+        }
+
+        XCTAssertFalse(sectionHeader.accessibilityHeaderEnabled)
     }
 }
